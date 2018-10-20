f754e8b4e6be1f1741cc43f8f37a3fb4872f6ef7
Chris Simpkins <jswu00@gmail.com>
2018-10-20 18:33:41 -0400

    Added IEX-API integration to get delayed stock prices

---
+++ quoteRetriever.py
@@ -0,0 +1,12 @@
+import sys
+import requests
+
+def getQuote(s):
+    URL = "https://api.iextrading.com/1.0/stock/" + s + "/delayed-quote"
+    response = requests.get(url = URL)
+    data = response.json()
+    price = data['delayedPrice']
+    return price
+
+quote = getQuote(sys.argv[1])
+print(sys.argv[1] + " is currently worth: $" + str(quote) + " USD. Please note that there is a 15-minute delay on stock-quotes.")
