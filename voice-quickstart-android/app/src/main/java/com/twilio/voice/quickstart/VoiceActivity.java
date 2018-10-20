7a5dae1906fac3f0d20608dd537eea747a2a063d
HemangRajvanshy <hemangrajvanshy@gmail.com>
2018-10-20 13:08:37 -0400

    some stuff idk

--- voice-quickstart-android/app/src/main/java/com/twilio/voice/quickstart/VoiceActivity.java
+++ voice-quickstart-android/app/src/main/java/com/twilio/voice/quickstart/VoiceActivity.java
@@ -59,7 +59,7 @@ public class VoiceActivity extends AppCompatActivity {
      *
      * For example : https://myurl.io/accessToken.php
      */
-    private static final String TWILIO_ACCESS_TOKEN_SERVER_URL = "http://8a4ec0c7.ngrock.io/accessToken";
+    private static final String TWILIO_ACCESS_TOKEN_SERVER_URL = "http://8a4ec0c7.ngrok.io/accessToken";
 
     private static final int MIC_PERMISSION_REQUEST_CODE = 1;
     private static final int SNACKBAR_DURATION = 4000;
